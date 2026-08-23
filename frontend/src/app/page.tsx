02feb79be2892dd85504faee5990ee72430f1c4b
unknown <israelcastillo14c@gmail.com>
2026-08-23 20:49:15 +0200

    page.tsx conectado a la API real de reservas con fetch y .map()

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,19 @@
-export default function Home(){
+import { Reserva } from "../types";
+import ReservaCard from "../components/ReservaCard";
+
+
+export default async function Home(){ 
+
+    const respuesta = await fetch("http://127.0.0.1:5000/api/reservas");
+    const datos = await respuesta.json(); 
+    console.log(datos)
+
     return (
-
-        <h1>
-            Hola mundo
-        </h1>
-
-    )
-
-};
-
-
-
-
+    <div>
+      {datos.map((reserva: Reserva) => (
+        <ReservaCard reserva={reserva} />
+      ))}
+    </div>
+  );
+  }
+  
