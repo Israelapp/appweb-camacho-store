b8c036c60d2e7304051d9818397cbca5f6272f20
Israel Castillo <israelcastillo14c@gmail.com>
2026-08-31 23:50:14 +0200

    Pagina de Inventario con fetch y ProductoCard, ProductoForm en progreso

--- frontend/src/app/inventario/page.tsx
+++ frontend/src/app/inventario/page.tsx
@@ -1,8 +1,17 @@
+import  { Producto } from "../../types";
+import ProductoCard from "../../components/ProductoCard";
 
+export default async function Inventario() {
 
-export default function InventarioPage() {
+    const respuesta = await fetch("http://127.0.0.1:5000/api/inventario");
+    const datos = await respuesta.json();
+    console.log(datos);
+
     return (
-        (<h1>Inventario</h1>)
-        
+        <div>
+            {datos.map((producto: Producto) => (
+                <ProductoCard key={producto.id} producto={producto} />
+            ))}
+        </div>
     );
 }
