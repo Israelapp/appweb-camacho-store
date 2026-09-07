4abbd9e5429b3edb9fb45ff1685266a6748cf954
Israel Castillo <israelcastillo14c@gmail.com>
2026-09-07 23:17:58 +0200

    Tipografia Inter y iconos con lucide-react en el Bottom Nav

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,13 @@
 import "./globals.css";
 import BottomNav from '../components/BottomNav';
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
 
     <html lang="es">
-      <body className="pb-20">
+      <body className={`${inter.className} pb-20`}>
     
         {children}
     
--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { ClipboardList, Package, BarChart3, Users, DollarSign } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -8,21 +8,28 @@ export default function BottomNav() {
 
   return (
     <div className="flex justify-between w-full bg-white p-4 fixed bottom-0 left-0">
-      <Link href="/reservas" className={pathname === "/reservas" ? "text-blue-600" : "text-gray-500"}>
+      <Link href="/reservas" className= {`flex flex-col items-center ${pathname === "/reservas" ? "text-blue-600" : "text-gray-500"}`}>
+        <ClipboardList size={20} />
         Reservas
       </Link>
-      <Link href="/inventario" className={pathname === "/inventario" ? "text-blue-600" : "text-gray-500"}>
+      <Link href="/inventario" className=  {`flex flex-col items-center ${pathname === "/inventario" ? "text-blue-600" : "text-gray-500"}`}>
+        <Package size= {20}/>
         Inventario
       </Link>
-      <Link href="/reportes" className={pathname === "/reportes" ? "text-blue-600" : "text-gray-500"}>
+      <Link href="/reportes" className= {`flex flex-col items-center ${pathname === "/reportes" ? "text-blue-600" : "text-gray-500"}`}>
+
+      <BarChart3 size= {20} />
         Reportes
       </Link>
-      <Link href="/clientes" className={pathname === "/clientes" ? "text-blue-600" : "text-gray-500"}>
+      <Link href="/clientes" className= {`flex flex-col items-center  ${pathname === "/clientes" ? "text-blue-600" : "text-gray-500"}`}>
+        <Users size={20}/>
         Clientes
       </Link>
 
-       <Link href="/pago" className={pathname === "/pago" ? "text-blue-600" : "text-gray-500"}>
-        Pagos
+       <Link href="/pago" className={`flex flex-col items-center  ${pathname === "/pago" ? "text-blue-600" : "text-gray-500"}`}>
+
+        <DollarSign size= {20} />
+          Pagos
       </Link>
     </div>
   );
